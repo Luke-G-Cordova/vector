88cb62a1192e374f8a69962d4fb4462f9a1dfa81
Luke Cordova <lukethegreat000@gmail.com>
2021-12-02 13:05:57 -0800

    adding more functions

--- src/Vector.js
+++ src/Vector.js
@@ -1,9 +1,8 @@
-
 export default class Vector {
     constructor(obj){
         this.x = obj.x;
         this.y = obj.y;
-        this.magnitude = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+        this.#calculateMagnitude();
     }
     static createNew(x, y){
         return new Vector({x: x, y: y});
@@ -11,22 +10,62 @@ export default class Vector {
     add(vector){
         this.x += vector.x;
         this.y += vector.y;
+        this.#calculateMagnitude();
+        return this;
+    }
+    addAngle(radians){
+        this.x = (this.x * Math.cos(radians) - (this.y * Math.sin(radians)));
+        this.y = (this.y * Math.cos(radians) + (this.x * Math.sin(radians)));
+        let mag = this.magnitude;
+        this.normalize();
+        this.mult(mag);
+        return this;
     }
     sub(vector){
         this.x -= vector.x;
         this.y -= vector.y;
+        this.#calculateMagnitude();
+        return this;
     }
     mult(scalor){
         this.x *= scalor;
         this.y *= scalor;
+        this.#calculateMagnitude();
+        return this;
     }
     div(scalor){
         this.x /= scalor;
         this.y /= scalor;
+        this.#calculateMagnitude();
+        return this;
+    }
+    upperLimit(scalor){
+        let f = Math.min(this.magnitude, scalor) / this.magnitude;
+        this.x = f * this.x;
+        this.y = f * this.y;
+        this.#calculateMagnitude();
+        return this;
+    }
+    lowerLimit(scalor){
+        let f = Math.max(this.magnitude, scalor) / this.magnitude;
+        this.x = f * this.x;
+        this.y = f * this.y;
+        this.#calculateMagnitude();
+        return this;
     }
     normalize(){
-        this.x /= this.magnitude;
-        this.y /= this.magnitude;
-        this.magnitude /= this.magnitude;
+        this.#calculateMagnitude();
+        if(this.magnitude !== 0){
+            this.x /= this.magnitude;
+            this.y /= this.magnitude;
+            this.#calculateMagnitude();
+        }
+        return this;
+    }
+    #calculateMagnitude(){
+        this.magnitude = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+    clone(){
+        return Vector.createNew(this.x, this.y);
     }
 }
